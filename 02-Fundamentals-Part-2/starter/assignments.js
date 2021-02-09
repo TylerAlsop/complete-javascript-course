ce89c2243cbee6960d5748921b31fd961339181d
Tyler Alsop <tylerjalsop@gmail.com>
2021-02-09 13:54:44 -0700

    Did functions assignment and rearranged the script order in the index.html file.

---
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -0,0 +1,8 @@
+/////////// Functions ///////////
+function describeCountry (country, population, capitalCity) {
+    console.log(`${country} has a population of ${population} million people and it's capitol city is ${capitalCity}.`)
+}
+
+describeCountry('The USA', 300, 'Washington D.C.')
+describeCountry('The Philippines', 110, 'Manila')
+describeCountry('France', 'who knows', 'Paris')
